7c64914fa83685a63aac9517e234fe6607cfa605
Tony Kung <lps.tony@gmail.com>
2017-02-23 12:00:40 -0600

    Initial Commit

---
+++ server.js
@@ -0,0 +1,39 @@
+var express = require("express");
+var app = express();
+var cookeiParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var ejs = require("ejs");
+var engine = require("ejs-mate");
+var session = require("express-session");
+var MongoStore = require('connect-mongo')(session);
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/rateme');
+
+app.use(express.static("public"));
+app.engine("ejs", engine);
+app.set("view engine", "ejs");
+app.use(cookeiParser());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use(session({
+  secret: 'Thisismytestkey.', 
+  resave: false,
+  saveUninitialized: false,
+  store: new MongoStore({mongooseConnection: mongoose.connection})
+}));
+
+app.get("/", function(req, res, next){
+  res.render('index');
+});
+
+app.get("/test", function(req, res, next){
+  res.render('test');
+});
+
+app.listen('3000', function(){
+  console.log("App is running on port 3000");
+});
+
+
