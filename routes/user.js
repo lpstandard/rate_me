3e10421ab88eaec9d0f3c5b7de18dd254449ba74
Tony Kung <lps.tony@gmail.com>
2017-03-04 19:31:25 -0600

    modified signup & login page

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,79 @@
-module.exports = (app) => {
+module.exports = (app, passport) => {
   app.get("/", function(req, res, next){
     res.render('index', {title: 'Index || RateMe'});
   });
 
   app.get('/signup', (req, res) => {
-    res.render('user/signup', {title: 'Sign up || RateMe'});
+    var errors = req.flash('error');
+    res.render('user/signup', {title: 'Sign up || RateMe', messages: errors, hasErrors: 
+    errors.length > 0 });
   });
+
+  app.post('/signup',validate, passport.authenticate('local.signup',{
+      successRedirect: '/',
+      failureRedirect: '/signup',
+      failureFlash: true
+    }));
 
   app.get('/login', (req, res) => {
-    res.render('user/login', {title: 'Login || RateMe'});
+    var errors = req.flash('error');
+    res.render('user/login', {title: 'Login || RateMe', messages: errors, hasErrors: 
+    errors.length > 0 });
   });
+
+  app.post('/login', loginValidation, passport.authenticate('local.login', {
+    successRedirect: '/home',
+    failureRedirect: '/login',
+    failureFlash: true
+  }));
+
+  app.get('/home', (req, res) => {
+    res.render('home', {title: "Home || RateMe"});
+  })
+
 }
 
+function validate(req, res, next) {
+  req.checkBody('fullname', 'Fullname is required').notEmpty();
+  req.checkBody('fullname', 'Fullname Must Not Be Less Than 5').isLength({min:5});
+  req.checkBody('email', 'Email is Required').notEmpty();
+  req.checkBody('email', 'Email is Invalid').isEmail();
+  req.checkBody('password', 'Password is Required').notEmpty();
+  req.checkBody('password', 'Password Must Not Be Less Than 5').isLength({min:5});
+  req.check("password", "Password must contain at least 1 number.").matches(/^(?=.*\d)(?=.*[a-z])[0-9a-z]{5,}$/, "i");
 
+  var errors = req.validationErrors();
+
+  if(errors){
+    var messages = [];
+    errors.forEach((error) => {
+      messages.push(error.msg);
+    });
+
+    req.flash('error', messages);
+    res.redirect('/signup')
+  } else {
+    return next();
+  }
+}
+
+function loginValidation(req, res, next) {
+  req.checkBody('email', 'Email is Required').notEmpty();
+  req.checkBody('email', 'Email is Invalid').isEmail();
+  req.checkBody('password', 'Password is Required').notEmpty();
+  req.checkBody('password', 'Password Must Not Be Less Than 5').isLength({min:5});
+  req.check("password", "Password must contain at least 1 number.").matches(/^(?=.*\d)(?=.*[a-z])[0-9a-z]{5,}$/, "i");
+
+  var loginErrors = req.validationErrors();
+
+  if(loginErrors){
+    var messages = [];
+    loginErrors.forEach((error) => {
+      messages.push(error.msg);
+    });
+    req.flash('error', messages);
+    res.redirect('/login')
+  } else {
+    return next();
+  }
+}
--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 var express = require("express");
-var app = express();
 var cookeiParser = require("cookie-parser");
 var bodyParser = require("body-parser");
+var validator = require("express-validator");
 var ejs = require("ejs");
 var engine = require("ejs-mate");
 var session = require("express-session");
 var MongoStore = require('connect-mongo')(session);
 var mongoose = require('mongoose');
+var passport = require("passport");
+var flash = require("connect-flash");
 
+var app = express();
+
+mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/rateme');
+
+require('./config/passport');
 
 app.use(express.static("public"));
 app.engine("ejs", engine);
@@ -17,6 +24,8 @@ app.use(cookeiParser());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+app.use(validator());
+
 app.use(session({
   secret: 'Thisismytestkey.', 
   resave: false,
@@ -24,7 +33,12 @@ app.use(session({
   store: new MongoStore({mongooseConnection: mongoose.connection})
 }));
 
-require('./routes/user')(app);
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/user')(app, passport);
 
 
 app.listen('3000', function(){
